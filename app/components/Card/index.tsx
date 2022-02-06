af1475b23bd31601672e9841f97394facda45bd1
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2022-02-06 20:46:48 +0900

    カードを表示させるページを作った

---
+++ app/components/Card/index.tsx
@@ -0,0 +1,39 @@
+import {
+    Box,
+    Text,
+    VStack,
+    Image,
+} from '@chakra-ui/react'
+
+export type CardProps = {
+  title: string;
+  url: string;
+}
+
+export default function Index({title, url}: CardProps) {
+    return (
+      <VStack
+        spacing="0px"
+        w="300px"
+        h="360px"
+        borderRadius="8px"
+        borderWidth="1px"
+        borderColor="gray.200"
+      >
+        <Box
+          pb="4px"
+          bg="yellow.100"
+        >
+          <Image
+            src={url}
+            w="100%"
+            h="300px"
+            objectFit="contain"
+            />
+          </Box>
+        <Text fontWeight="bold" p="16px">
+          {title}
+        </Text>
+      </VStack>
+    )
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,51 @@
 import {
   Box,
+  Flex,
+  Text,
 } from '@chakra-ui/react'
+import Card from '../components/Card';
+import type { CardProps } from '../components/Card';
+import { useLoaderData } from 'remix';
+
+export const loader = async () => {
+  const mock: CardProps[] = [
+    {
+      title: '天丼',
+      url: 'https://1.bp.blogspot.com/-R-93rk6Bfqc/YHDkI0o9c6I/AAAAAAABdlA/hL5egaLPavYrBmEGVsltrlYgyHuAu-sQgCNcBGAsYHQ/s400/food_udon_goboten.png'
+    },
+    {
+      title: '天丼',
+      url: 'https://1.bp.blogspot.com/-R-93rk6Bfqc/YHDkI0o9c6I/AAAAAAABdlA/hL5egaLPavYrBmEGVsltrlYgyHuAu-sQgCNcBGAsYHQ/s400/food_udon_goboten.png'
+    },
+    {
+      title: '天丼',
+      url: 'https://1.bp.blogspot.com/-R-93rk6Bfqc/YHDkI0o9c6I/AAAAAAABdlA/hL5egaLPavYrBmEGVsltrlYgyHuAu-sQgCNcBGAsYHQ/s400/food_udon_goboten.png'
+    },
+    {
+      title: '天丼',
+      url: 'https://1.bp.blogspot.com/-R-93rk6Bfqc/YHDkI0o9c6I/AAAAAAABdlA/hL5egaLPavYrBmEGVsltrlYgyHuAu-sQgCNcBGAsYHQ/s400/food_udon_goboten.png'
+    },
+    {
+      title: '天丼',
+      url: 'https://1.bp.blogspot.com/-R-93rk6Bfqc/YHDkI0o9c6I/AAAAAAABdlA/hL5egaLPavYrBmEGVsltrlYgyHuAu-sQgCNcBGAsYHQ/s400/food_udon_goboten.png'
+    },
+  ]
+  return mock;
+};
 
 export default function Index() {
+  const data = useLoaderData<CardProps[]>();
+  console.log(data)
 
   return (
-    <Box>
-      ぽけもんが表示されるよ
-    </Box>
+    <Flex wrap="wrap" py="24px">
+      {data.map((item, index) =>
+        (
+          <Box mr="16px" mb="16px">
+            <Card key={index} {...item} />
+          </Box>
+        )
+      )}
+    </Flex>
   );
 }
