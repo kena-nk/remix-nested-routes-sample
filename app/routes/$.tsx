7d575b063b1ec78883247030bdba7d56e336f9c1
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2022-02-12 13:12:43 +0900

    存在しないURLにアクセスした時用のページを用意した

---
+++ app/routes/$.tsx
@@ -0,0 +1,28 @@
+import {
+  Box,
+  Flex,
+  Spacer,
+  VStack,
+  Button,
+  Text,
+} from '@chakra-ui/react';
+import {
+  useNavigate,
+} from 'remix';
+
+const Index = () => {
+  const navigate = useNavigate();
+
+  return (
+    <VStack spacing="40px" p="40px">
+      <Text variant="dotBody1">
+        存在しないURLにアクセスしたな
+      </Text>
+      <Button onClick={() => navigate('/pokemon')}>
+        TOPにもどる
+      </Button>
+    </VStack>
+  );
+};
+
+export default Index;
